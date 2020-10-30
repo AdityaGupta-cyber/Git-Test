eae442262b04e80372f1d62d107cbf99de876973
AdityaGupta-cyber <73712794+AdityaGupta-cyber@users.noreply.github.com>
2020-10-30 23:21:19 +0530

    Swap 2 numbers In C

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=1;
+    printf("%d\n", a++);
+    a=1;
+    printf("%d\n", ++a);
+    a=1;
+    printf("%d\n", a++ + ++a);
+    a=1;
+    printf("%d\n", --a + a--);
+    return 0;
+}
